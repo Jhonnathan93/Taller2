b3c3ff38bb33765663b83c401f07df7c036884cf
Jhonnathan93 <jsocampod@eafit.edu.co>
2024-05-06 15:44:10 -0500

    Finish first route

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const pokeneaRoutes = require('./routes/routes');
 
-app.use('/pokenea', pokeneaRoutes);
+app.use('/', pokeneaRoutes);
 
 app.listen(3000, () => {
     console.log('Servidor iniciado en el puerto 3000');
